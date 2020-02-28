d4f94b300ab262b7075f474513548610dc43995c
ebuzey <51092125+ebuzey@users.noreply.github.com>
2020-02-28 00:06:36 -0300

    Add files via upload

---
+++ arreglos.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+  //var arreglo [10]int
+  // arreglo := [3]int{1,2,3}
+  // for i := 0; i < len(arreglo); i++ {
+  //   fmt.Println(arreglo[i])
+  // }
+  var matriz [3][2]int
+  matriz[0][1] = 20
+  fmt.Println(matriz)
+}
